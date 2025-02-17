e0510cd85b1968d78d4fec9efa1abc10cfbe70f2
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-02-18 01:53:07 +0530

    create book slice

---
+++ src/Slices/BookSlice.ts
@@ -0,0 +1,33 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {Book} from "../interface/Book.ts";
+
+
+const initialState : Book[] = []
+
+const BookSlice = createSlice({
+    name: "book",
+    initialState: initialState,
+    reducers: {
+        saveBook:(state,action)=>{
+            state.push(action.payload)
+        },
+        updateBook:(state, action )=>{
+            const updateBooks = action.payload
+            const index = state.findIndex((book) =>book.id  === updateBooks.id);
+            if (index !== -1) {
+                state[index] = { ...state[index], ...updateBooks };
+            }
+        },
+        deleteBook:(state, action )=>{
+            const index = action.payload
+            if (index !== -1) {
+                state.splice(index, 1);
+            }
+        },
+
+    }
+})
+
+export const {saveBook,updateBook,deleteBook}=BookSlice.actions
+
+export default BookSlice.reducer
